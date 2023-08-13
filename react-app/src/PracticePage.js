a85a50aec23908c5427a5b09a2a26f8097414497
Advika Suri <asuri010@ucr.edu>
2023-08-13 08:25:23 -0700

    added a single card with no bugs. now need to go through multiple cards

--- react-app/src/PracticePage.js
+++ react-app/src/PracticePage.js
@@ -1,22 +1,29 @@
-import React, {useState} from 'react';
-import { Box, Button } from "@chakra-ui/react";
-import backgroundImage from'./backgroundImage.png';
-import {PracticeButton} from './CurvedButton';
-
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import backgroundImage from './backgroundImage.png';
+import { PracticeButton } from './CurvedButton';
+import Flashcard from './Flashcard'; // Import the Flashcard component
 
 const PracticePage = () => {
   return (
     <>
-    <PracticeButton to="/">Exit Practice</PracticeButton>
-    <Box
-            w="100wh"
-            h="100vh"
-            bgImage={backgroundImage}
-            bgPosition="center"
-            bgRepeat="no-repeat"
-            bgSize="cover"   
-    >
-    </Box>
+      <PracticeButton to="/">Exit Practice</PracticeButton>
+      <Box
+        w="100wh"
+        h="100vh"
+        bgImage={backgroundImage}
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Flashcard
+          frontContent={<div>Front of the Card</div>} // Customize the front content
+          backContent={<div>Back of the Card</div>}   // Customize the back content
+        />
+      </Box>
     </>
   );
 };
--- react-app/src/curvedButton.js
+++ react-app/src/curvedButton.js
@@ -50,7 +50,7 @@ export const PracticeButton = ({ children, to, ...rest }) => {
       color="white"
       fontSize="xl"
       fontWeight="normal"
-      style={{ position: 'absolute', top: '90px', left: '0px', zIndex: '100' }}
+      style={{ position: 'absolute', top: '100px', left: '0px', zIndex: '100' }}
       onClick={handleClick}
       {...rest} // Pass any additional props to the underlying Chakra Button component
     >
